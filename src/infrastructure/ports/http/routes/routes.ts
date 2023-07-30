3439bb7e1d4c224e9a6c4088a25b4e4143d0e237
Ícaro Roberto Vieira Santos <icaro.santos@fretebras.com.br>
2023-07-30 16:08:00 -0300

    hackathon project

--- src/infrastructure/ports/http/routes/routes.ts
+++ src/infrastructure/ports/http/routes/routes.ts
@@ -1,9 +1,13 @@
 import {Router as ExpressRouter} from 'express';
 import {MessageController} from '@http/controllers/message.controller';
+import {RootController} from '@http/controllers/root.controller';
 
 export class Router {
 	public static config(): ExpressRouter {
 		const route = ExpressRouter();
+
+		// Root routes
+		route.get('/check', RootController.healthCheck);
 
 		// Message routes
 		route.post('/messages/flow-from-speech', MessageController.flowFromSpeech);
